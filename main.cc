184e0606db73eb04503281fa3a4e9c151cafce8f
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-09-11 16:08:06 -0500

    .

--- main.cc
+++ main.cc
@@ -1,10 +1,10 @@
 import civlike;
-
-#include <cstdio>
+import uiproto;
 
 int main()
 {
     const GameParameters par;
     const Ruleset ruleset = Ruleset::create_from_cpp(colonization_ruleset());
     Game game(ruleset, par);
+    UIProto(Nation::Id { 0 }).run(game);
 }
