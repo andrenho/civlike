8a66ae694b7e850ddca9b01e2c599774616b840f
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-09-11 13:23:58 -0500

    .

--- main.cc
+++ main.cc
@@ -1,5 +1,12 @@
-import hello;
+import geometry;
+import ruleset;
+import colonization;
 
-int main() {
-    hello_world();
+#include <cstdio>
+
+int main()
+{
+    GameParameters par;
+    Ruleset ruleset = Ruleset::create_from_cpp(colonization_ruleset());
+    printf("%d\n", ruleset.map_size.optional(ruleset, par)->w);
 }
