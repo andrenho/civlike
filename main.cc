2323e7b2a4f60c80270c530e603876b7acae5006
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-09-11 15:42:02 -0500

    .

--- main.cc
+++ main.cc
@@ -6,5 +6,5 @@ int main()
 {
     const GameParameters par;
     const Ruleset ruleset = Ruleset::create_from_cpp(colonization_ruleset());
-    printf("%d\n", ruleset.map_size.optional(ruleset, par)->w);
+    Game game(ruleset, par);
 }
