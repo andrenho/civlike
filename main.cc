01db369b2d59dd5480ef5440c3e80612ac98f5c8
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-09-11 14:04:51 -0500

    .

--- main.cc
+++ main.cc
@@ -1,12 +1,10 @@
-import geometry;
-import ruleset;
-import colonization;
+import civlike;
 
 #include <cstdio>
 
 int main()
 {
-    GameParameters par;
-    Ruleset ruleset = Ruleset::create_from_cpp(colonization_ruleset());
+    const GameParameters par;
+    const Ruleset ruleset = Ruleset::create_from_cpp(colonization_ruleset());
     printf("%d\n", ruleset.map_size.optional(ruleset, par)->w);
 }
